7a8813a89b8e2e10bd8ed8624f61f6f4672f5789
utente <utente@DESKTOP-I2RGQG1.station>
2023-09-14 19:35:15 +0200

    Ciao mondo!

--- src/prova/ProvaTest.java
+++ src/prova/ProvaTest.java
@@ -16,6 +16,7 @@ public class ProvaTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ciao mondo!");
     }
     
 }
